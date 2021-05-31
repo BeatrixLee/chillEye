415056a9dbb8a0ccd9a7d65e62add1cc350c217e
Beatrix <blsr3@academy.cin.ufpe.br>
2021-05-31 10:26:41 -0300

    constroi interface dos exercícios

--- chillEye/chillEye WatchKit Extension/ContentView.swift
+++ chillEye/chillEye WatchKit Extension/ContentView.swift
@@ -9,8 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            
+            
+            HStack {
+                
+                
+                Text("Diagonal").font(.system(size: 16, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 1, green: 0.9, blue: 0.13, alpha: 1)))
+                    .padding(.trailing, 40)
+                
+                
+                Text("0:55").font(.system(size: 16, weight: .semibold)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).multilineTextAlignment(.trailing)
+            }
+            .padding(.bottom, 20)
+            
+            VStack {
+                
+                Text("animation placeholder")
+                    .padding(.bottom, 20)
+                
+                Text("Mova seus olhos \nna diagonal")
+                    
+                    .font(.footnote)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .frame(width: 116, height: 35, alignment: .center)
+                    .padding(EdgeInsets.init(top: 5, leading: 0, bottom: 0, trailing: 0))
+                
+            }
+            .padding(.bottom, 10)
+            
+            ZStack {
+                
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color(#colorLiteral(red: 0.1411764770746231, green: 0.1411764770746231, blue: 0.1411764770746231, alpha: 1)))
+                    .frame(width: 44, height: 18)
+                
+                Text("1/3").font(.system(size: 13, weight: .regular)).foregroundColor(Color(#colorLiteral(red: 1, green: 0.9, blue: 0.13, alpha: 1))).multilineTextAlignment(.trailing)
+                
+            }
+            
+            
+            
+        }
+        .padding(EdgeInsets.init(top: 20, leading: 10, bottom: 10, trailing: 10))
     }
 }
 
