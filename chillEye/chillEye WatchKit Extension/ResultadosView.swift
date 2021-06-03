9e69da48d12579b3ef2b24767ed405b53bd2e7af
Meyrillan <meyrillan@gmail.com>
2021-06-03 10:40:24 -0300

    Adiciona ajustes na UI

--- chillEye/chillEye WatchKit Extension/ResultadosView.swift
+++ chillEye/chillEye WatchKit Extension/ResultadosView.swift
@@ -8,60 +8,57 @@
 import SwiftUI
 
 struct ResultadosView: View {
-
+    
     var body: some View {
         VStack{
             
             HStack{
-                Image(iconeResultado)
+                Image("iconeResultado")
                     .resizable()
+                    .scaledToFit()
                     .frame(width: 80, height: 40)
-
+                
                 VStack{
-                    
-            //TEMPO DE HOJE
-            Text("TEMPO DE HOJE")
-                    .font(.system(size: 12, weight: .medium))
-                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-                    .multilineTextAlignment(.center)
-                    .padding(.bottom, 2)
-                    
-            //1MIN
-            Text("1")
-    
-                .font(.system(size: 24, weight: .medium, design: .rounded))
-                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07) + Text("MIN")
-                .font(.system(size: 18, weight: .medium, design: .rounded))
-                .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07)
-                
-                
-            }
-
-                
-                ZStack {
-                    
-
-                    Circle()
-                    .fill(Color(#colorLiteral(red: 0, green: 0.9607843160629272, blue: 0.9176470637321472, alpha: 1)))
-                    .frame(width: 20, height: 20)
-                    
-                    Text("S").font(.system(size: 13, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))).multilineTextAlignment(.trailing)
-                    
-                    Circle()
-                    .fill(Color(#colorLiteral(red: 0, green: 0.9607843160629272, blue: 0.9176470637321472, alpha: 1)))
-                    .frame(width: 20, height: 20)
-                    
-                    Text("D").font(.system(size: 13, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))).multilineTextAlignment(.trailing)
+                    //TEMPO DE HOJE
+                    Text("TEMPO DE HOJE")
+                        .font(.system(size: 12, weight: .medium))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom, 2)
                     
                     
-                    
+                    //1MIN
+                    Text("1")
+                        
+                        .font(.system(size: 24, weight: .medium, design: .rounded))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07) + Text("MIN")
+                        .font(.system(size: 18, weight: .medium, design: .rounded))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07)
                 }
-                //DADOS SEMANAIS
-                Text("DADOS SEMANAIS").font(.system(size: 15, weight: .medium, design: .rounded)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07)
-            
-                
-
             }
+            Divider()
+            
+            //DADOS SEMANAIS
+            Text("DADOS SEMANAIS").font(.system(size: 15, weight: .medium, design: .rounded)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))).tracking(0.07)
+            
+        HStack {
+                
+                
+                Circle()
+                    .fill(Color(#colorLiteral(red: 0, green: 0.9607843160629272, blue: 0.9176470637321472, alpha: 1)))
+                    .frame(width: 20, height: 20)
+                
+                Text("S").font(.system(size: 13, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))).multilineTextAlignment(.trailing)
+                
+                Circle()
+                    .fill(Color(#colorLiteral(red: 0, green: 0.9607843160629272, blue: 0.9176470637321472, alpha: 1)))
+                    .frame(width: 20, height: 20)
+                
+                Text("D").font(.system(size: 13, weight: .medium)).foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))).multilineTextAlignment(.trailing)
+                
+                
+            }
+            
             
         }
     }
